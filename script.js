673cf5ab9b369692910c5506fa9e4b06040c19f6
Manuela <manuelarosin22@gmail.com>
2025-06-23 14:57:44 -0300

    Arrumando

---
+++ script.js
@@ -0,0 +1,27 @@
+let cerejas = 0;
+let cerejasPorClique = 1;
+let upgradesComprados = 0;
+
+function atualizarTela() {
+    document.getElementById('cerejas').innerText = `Cerejas: ${cerejas}/500`;
+    document.getElementById('cerejasPorClique').innerText = `Cerejas por clique: ${cerejasPorClique}`;
+    document.getElementById('upgrades').innerText = `Upgrades: ${upgradesComprados}`;
+}
+
+document.getElementById('cherryImage').addEventListener('click', () => {
+    cerejas += cerejasPorClique;
+    atualizarTela();
+});
+
+function comprarUpgrade(custo, aumento) {
+    if (cerejas >= custo) {
+        cerejas -= custo;
+        cerejasPorClique += aumento;
+        upgradesComprados++;
+        atualizarTela();
+    } else {
+        alert("Você não tem cerejas suficientes!");
+    }
+}
+
+atualizarTela();
